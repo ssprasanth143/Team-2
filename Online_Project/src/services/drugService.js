3fd5c56cd53922150f6428c9b0a219c9901f3022
Prasanth Saravanan <114046985+ssprasanth143@users.noreply.github.com>
2025-08-08 12:03:21 +0530

    Delete Online_Project directory

--- Online_Project/src/services/drugService.js
+++
@@ -1,4 +0,0 @@
-import api from "./api";
-
-export const fetchAllDrugs = () => api.get("/drugs");
-export const fetchDrugById = (id) => api.get(`/drugs/${id}`);
--- Online_Project/src/services/orderService.js
+++
@@ -1,7 +0,0 @@
-import api from './api';
-
-export const addToCart = (drugId) =>
-  api.post('/cart/add', { drugId });
-
-export const checkoutCart = (items) =>
-  api.post('/cart/checkout', { items });
